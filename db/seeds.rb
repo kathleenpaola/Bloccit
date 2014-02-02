df68d3acfa0d8522a5ac5b422d98abcef4d15553
Kathleen <kathleen.paola@gmail.com>
2014-02-02 12:05:09 -0500

    Associated and nested topics and posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,46 @@
 require 'faker'
 
+  topics = []
+  15.times do
+    topics << Topic.create(
+      name: Faker::Lorem.words(rand(1..10)).join(" "),
+      description: Faker::Lorem.paragraph(rand(1..4))
+      )
+  end
+
+  u = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'admin')
+
+u = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com',
+  password: 'helloworld',
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'moderator')
+
+u = User.new(
+  name: 'Member User',
+  email: 'member@example.com',
+  password: 'helloworld',
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+
+"Seed finished"
+"#{User.count} users created" 
+"#{Post.count} posts created"
+"#{Comment.count} comments created"
+
 rand(4..10).times do 
+
   password = Faker::Lorem.characters(10)
   u = User.new(
     name: Faker::Name.name,
@@ -10,27 +50,25 @@ rand(4..10).times do
   u.skip_confirmation!
   u.save
 
+  end 
 
   rand(5..12).times do 
+    topic = topics.first
     p = u.posts.create(
+      topic: topic,
       title: Faker::Lorem.words(rand(1..10)).join(" "),
       body: Faker::Lorem.paragraphs(rand(1..4)).join("/n"))
 
     p.update_attribute(:created_at, Time.now - rand(600..31536000))
 
-
-  rand(3..7).times do
-    p.comments.create(
+    rand(3..7).times do
+      p.comments.create(
         body: Faker::Lorem.paragraphs(rand(1..2)).join("/n"))
-   end       
+    end       
+
+    topics.rotate!
+    
   end
-end
 
-u = User.first
-u.skip_reconfirmation!
-u.update_attributes(email: 'kathleen.paola@gmail.com', password: 'helloworld', password_confirmation: 'helloworld')
 
-"Seed finished"
-"#{User.count} users created" 
-"#{Post.count} posts created"
-"#{Comment.count} comments created"
+
