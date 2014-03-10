5b3191eba0f742e7da5780d01db45464ddea4bdc
Kathleen <kathleen.paola@gmail.com>
2014-03-10 19:54:26 -0400

    Worked through with Aaron to correct errors

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -6,9 +6,9 @@ class FavoriteMailer < ActionMailer::Base
     @post = post
     @comment = comment
 
-    headers["Message-ID"] = "<comments/#{@comment.id}@bloccit.example>"
-    headers["In-Reply-To"] = "<post/#{@post.id}@bloccit.example>"
-    headers["References"] = "<post/#{@post.id}@bloccit.example>"
+    headers["Message-ID"] = "<comments/#{@comment.id}@kathleenpaolabloccit.herokuapp.com>"
+    headers["In-Reply-To"] = "<post/#{@post.id}@kathleenpaolabloccit.herokuapp.com>"
+    headers["References"] = "<post/#{@post.id}@kathleenpaolabloccit.herokuapp.com>"
 
     mail(to: user.email, subject: "New comment on #{post.title}")
   end
